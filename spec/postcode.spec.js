bcef1395c44a79c7aedd9b5c4d5bbc0a0bef261c
nmiddleton <ngmiddleton@hotmail.com>
2014-07-02 16:21:59 +0100

    log_with Bind

--- spec/postcode.spec.js
+++ spec/postcode.spec.js
@@ -4,7 +4,7 @@ var expect   = require('chai').expect,
 
     Postcode = require('../src/postcode');
 
-describe.skip('Postcode', function() {
+describe.only ('Postcode', function() {
 
     it('is instantiated with a string representing a post code', function() {
         // prepare
@@ -17,10 +17,60 @@ describe.skip('Postcode', function() {
         // assert
         expect(postcode).to.be.an('object');
     });
+    it('fails an empty string', function() {
+        // prepare
 
+        // expect
+
+        // act
+        var postcode = new Postcode();
+        // assert
+        expect(postcode).to.be.an('object');
+    });
     describe('a valid post code', function() {
-
-        it('...', function() {
+        it('Fails a bad content outward Postcode ', function() {
+            var postcode = new Postcode('EEE 55P');
+            expect(postcode.IsValid()).to.be.false;
+        });
+        it('Fails a bad length outward Postcode ', function() {
+            var postcode = new Postcode('E 55P');
+            expect(postcode.IsValid()).to.be.false;
+        });
+        it('Fails a bad content inward Postcode ', function() {
+            var postcode = new Postcode('E14 55P');
+            expect(postcode.IsValid()).to.be.false;
+        });
+        it('Fails a bad length inward Postcode', function() {
+            var postcode = new Postcode('E14 5EPP');
+            expect(postcode.IsValid()).to.be.false;
+        });
+        it('Matches a good Postcode', function() {
+            var postcode = new Postcode('E14 5BP');
+            expect(postcode.IsValid()).to.be.true;
+        });
+        it('Matches a good Postcode', function() {
+            var postcode = new Postcode('EC1A 1BB');
+            expect(postcode.IsValid()).to.be.true;
+        });
+        it('Matches a good Postcode', function() {
+            var postcode = new Postcode('W1A 1HQ');
+            expect(postcode.IsValid()).to.be.true;
+        });
+        it('Matches a good Postcode', function() {
+            var postcode = new Postcode('M1 1AA');
+            expect(postcode.IsValid()).to.be.true;
+        });
+        it('Matches a good Postcode', function() {
+            var postcode = new Postcode('B33 8TH');
+            expect(postcode.IsValid()).to.be.true;
+        });
+        it('Matches a good Postcode', function() {
+            var postcode = new Postcode('CR2 6XH');
+            expect(postcode.IsValid()).to.be.true;
+        });
+        it('Matches a good Postcode', function() {
+            var postcode = new Postcode('DN55 1PT');
+            expect(postcode.IsValid()).to.be.true;
         });
     });
 
--- src/postcode.js
+++ src/postcode.js
@@ -1,1 +1,54 @@
-module.exports = undefined;
+
+//AA9A_9AA = 'EC1A 1BB';
+//A9A_9AA	 = 'W1A 1HQ';
+//A9_9AA	 = 'M1 1AA';
+//A99_9AA	 = 'B33 8TH';
+//AA9_9AA	 = 'CR2 6XH';
+//AA99_9AA = 'DN55 1PT';
+
+'use strict';
+module.exports = function Postcode(input) {
+    var args = Array.prototype.slice.call(arguments,0),  //needed to test empty
+        regALPHA = /^[a-zA-Z]+$/,
+        regNUM = /^[0-9]+$/g,
+        regAPLHANUM = /^[0-9a-zA-Z]+$/,
+        code, inward, outward;
+
+    var textWellFormed = function () {
+        return args.length > 0 && input.indexOf(' ') != -1;
+    };
+    if (textWellFormed()) {
+        code = input.split(' ', 2);
+        outward = code[0];
+        inward = code[1];
+    }
+
+    var inwardIsValid = function () {
+        return  new RegExp(regNUM).test(inward.substr(0, 1)) &&
+                new RegExp(regALPHA).test(inward.substr(1, 2)) &&
+                inward.length === 3;
+    };
+
+    var outwardIsValid = function () {
+        return  new RegExp(regALPHA).test(outward.substr(0, 1)) &&      // 1st is letter
+                new RegExp(regAPLHANUM).test(outward.substr(1, 1)) &&   // 2nd is alphanum
+                outward.match(regNUM)||[].length >0 &&                  // there has been at least one number
+                outward.match(regNUM)||[].length <3 &&                  // ..but not more than two in a row.
+                outward.match(regALPHA)||[].length <3;                  // ..same for letters - not more than two in a row
+    };
+    // Return from the object
+    return {
+        IsValid: function () {
+            return  textWellFormed() && inwardIsValid() && outwardIsValid();
+
+        }
+    };
+
+
+
+
+
+        //function isNum (aNum) { return new RegExp(regNUM).test(aNum) };
+    //function isAZ  (txt) { return new RegExp(regAPLHA).test(txt) };
+
+};
